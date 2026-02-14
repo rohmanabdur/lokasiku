18c6b82ab74a335a00b8f71502a3a3a1412eccab
Abdur Rohman <masabdrohman@gmail.com>
2026-02-14 19:11:13 +0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ petaku.locate({
   setView: true,
   maxZoom: 16,
 });
-const kunci = "DVdOJe9GdF0uFWdlayhk";
+const kunci = "XXXXXXXXXX";
 const lapisan = L.tileLayer(
   `https://api.maptiler.com/maps/streets-v2/{z}/{x}/{y}.png?key=${kunci}`,
   {
